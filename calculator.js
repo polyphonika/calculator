b7dd237443eff33ec4b1951eeb8c0dd964e16047
Ryan Berry <email@cornerhousemedia.com>
2024-10-03 17:35:23 +0100

    initial calculator html  scaffolding

--- calculator.js
+++ calculator.js
@@ -6,3 +6,27 @@ const divide = (a,b) => a/b;
 const multiply = (a,b) => a*b;
 
 // TODO: add scientific functions later or at least extras such as exponents
+// TODO: Handle negatives, NaN values, errors
+
+// console.log(add(-5,3));
+// console.log(subtract(5,-3));
+// console.log(divide(6,4));
+// console.log(multiply(5,3))
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+const operate = (a,o,b) => {
+    const operations = {
+        '+': add,
+        '-': subtract,
+        '/': divide,
+        '*': multiply
+    };
+
+    return operations[o] ? operations[o](a, b) : null;
+}
+
+// console.log(operate(5,'*',3));
+// console.log(1/6);
