ab321456d718cebc7fdb7a0d4bf1c14839cdd64e
Ryan Berry <email@cornerhousemedia.com>
2024-10-03 16:21:06 +0100

    initial commit with basic math functions

---
+++ calculator.js
@@ -0,0 +1,8 @@
+/* create initial functions: add, subtract, divide, multiply */
+
+const add = (a,b) => a+b;
+const subtract = (a,b) => a-b;
+const divide = (a,b) => a/b;
+const multiply = (a,b) => a*b;
+
+// TODO: add scientific functions later or at least extras such as exponents
