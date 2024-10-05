74e01e46f80b8d3fdb7ffede4571f6302483f381
Ryan Berry <email@cornerhousemedia.com>
2024-10-05 12:22:15 +0100

    working logic, positive integers only

--- calculator.js
+++ calculator.js
@@ -1,3 +1,12 @@
+let firstNumber = []
+let secondNumber = []
+let operator;
+let postCalcFlag;
+let displayValue = [];
+
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('.button');
+
 /* create initial functions: add, subtract, divide, multiply */
 
 const add = (a,b) => a+b;
@@ -5,59 +14,110 @@ const subtract = (a,b) => a-b;
 const divide = (a,b) => a/b;
 const multiply = (a,b) => a*b;
 
-// TODO: add scientific functions later or at least extras such as exponents
-// TODO: Handle negatives, NaN values, errors
-
-// console.log(add(-5,3));
-// console.log(subtract(5,-3));
-// console.log(divide(6,4));
-// console.log(multiply(5,3))
-
-let firstNumber;
-let secondNumber;
-let operator;
-
 const operate = (a,o,b) => {
     const operations = {
         '+': add,
         '-': subtract,
-        '/': divide,
-        '*': multiply
+        '÷': divide,
+        'x': multiply
     };
 
     return operations[o] ? operations[o](a, b) : null;
 }
 
-// console.log(operate(5,'*',3));
-// console.log(1/6);
-
-const display = document.getElementById('display');
-// const buttons = document.getElementsByClassName('button');
-const buttons = document.querySelectorAll('.button');
-
+// button listeners
 const btnPress = buttons.forEach(element => {
     element.addEventListener('click', function(e) {
         const btnTextValue = e.target.textContent;
-        if (/^\d+$/.test(btnTextValue)) { 
-        console.log('clicked');
-        //add number to displayValue, update display
-        updateDisplay(btnTextValue);
-        };
-    });
-  
+        
+        // number presses
+        if (/^\d+$/.test(btnTextValue)) {
+            pressNum(btnTextValue);
+        }
+
+        // if operator pressed
+        if (/^[+\-x/÷]$/.test(btnTextValue)) {
+            pressOperator(btnTextValue);           
+        }      
+        
+        // if equals pressed
+        if (btnTextValue==='=') {
+           pressEquals();
+        }        
+
+    });  
 
 })
+
+function pressNum(btnTextValue) {
+    console.log('num press');
+
+    // reset if post calculation so we start again
+    if(!operator && postCalcFlag) {
+        firstNumber=[];
+        displayValue=[];
+    }
+    
+    // no leading zeros
+    if (!operator) {
+        postCalcFlag=false;
+        if (!(btnTextValue==0 && !firstNumber.length)) { //no leading zeroes
+            firstNumber.push(parseInt(btnTextValue))
+            updateDisplay(btnTextValue);
+        }
+    } else {
+        secondNumber.push(parseInt(btnTextValue));
+        updateDisplay(btnTextValue);
+    }
+
+    //!operator ? firstNumber.push(parseInt(btnTextValue)) : secondNumber.push(parseInt(btnTextValue));
+    // updateDisplay(btnTextValue);
+}
+
+function pressEquals(btnTextValue) {
+    outputAll('testing multi ops');
+    if(secondNumber.length) {
+        firstNumber = operate(parseInt(firstNumber.join('')),operator,parseInt(secondNumber.join('')));
+        displayValue = [];
+        updateDisplay(firstNumber);    
+        firstNumber = [firstNumber];
+        secondNumber = [];
+        postCalcFlag = true;
         
+        // if another operator instead of equals, clear screen
+        if (btnTextValue) {
+            displayValue = [];
+        }
+        // reset operator if equals, or store the next operator
+        operator = btnTextValue || '';
+    }
+}
 
-let displayValue = [];
-
-// update display
-function updateDisplay(num) {
+function pressOperator(btnTextValue) {
+    if(firstNumber.length) {        
+        if(!operator) {
+        operator = btnTextValue;
+        displayValue=[]
+        console.log('op press');
+        } else if (operator && secondNumber.length) {
+            pressEquals(btnTextValue);
+        }
+    }
+    // updateDisplay(btnTextValue); 
+}
+ 
+function updateDisplay(char) {
     const displayElement = document.getElementById('display');
-    displayValue.push(parseInt(num));
+    displayValue.push(char);
     displayElement.textContent = displayValue.join('');    
 }
 
-//TODO: thoughts on handling leading zero, test with real calculators
-//TODO: max screen width, scrolling
+function outputAll(label) {
+    console.log('------------------------');
+    console.log(label || 'unknown run');
+    console.log('First Number: ' + firstNumber + ', ' + typeof firstNumber);
+    console.log('Second Number: ' + secondNumber + ', ' + typeof secondNumber);
+    console.log('Operator: ' + operator);
+    console.log('Display Value: ' + displayValue + ', ' + typeof displayValue);
+}
 
---
+++ notes.md
@@ -0,0 +1,24 @@
+Calculator HTML
+---------------
+
+Next steps:
+* ~~What happens if operator pressed first: NaN~~
+* ~~What happens if operator pressed more than once~~
+* ~~What happens if don't press equals and have a second operator instead~~
+* ~~What happens if we press a number after equals? It should reset~~
+* ~~What happens if leading with zero~~
+* ~~Shouldn't be able to lead with zero, ignore it.~~
+* ~~what if equals pressed to early~~
+* git 
+* Review project, probably move to floats at this point and negative numbers
+
+**Bugs** 
+
+
+******************************
+Scratch area:
+
+
+
+
+
